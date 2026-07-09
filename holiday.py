d5480e6eb937a81beb64b34ca14f91b2c4ac8009
s112002159-commits <s112002159@g.ksu.edu.tw>
2026-07-09 09:48:45 +0800

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -1,15 +1,45 @@
+
 import holidays
 import datetime
+import pytz
+
+taiwan_tz = pytz.timezone(
+    "Asia/Taipei"
+)
 
 tw_holidays = holidays.Taiwan()
 
-def is_tomorrow_workday():
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+def now():
 
-    if tomorrow.weekday() >= 5:
+    return datetime.datetime.now(
+        taiwan_tz
+    )
+
+def today():
+
+    return now().date()
+
+def tomorrow():
+
+    return (
+        today()
+        + datetime.timedelta(days=1)
+    )
+
+def is_workday(date_obj):
+
+    if date_obj.weekday() >= 5:
+
         return False
 
-    if tomorrow in tw_holidays:
+    if date_obj in tw_holidays:
+
         return False
 
     return True
+
+def is_tomorrow_workday():
+
+    return is_workday(
+        tomorrow()
+    )
