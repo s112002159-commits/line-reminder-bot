0a7d8cd9949787ffa7b88732c8aee9de1b154c4a
s112002159-commits <s112002159@g.ksu.edu.tw>
2026-07-09 10:46:28 +0800

    Update line_handler.py

--- line_handler.py
+++ line_handler.py
@@ -1,9 +1,49 @@
-from linebot.models import TextMessage,TextSendMessage
-from commands import command
-from storage import load,save
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+
+from config import (
+    LINE_CHANNEL_ACCESS_TOKEN
+)
+
 
 def handle_event(event):
- if isinstance(event.message,TextMessage):
-  txt=event.message.text
-  r=command(txt)
-  if r: event.reply_token and event.reply(TextSendMessage(r))
+
+
+    text = event.message.text
+
+
+    reply = "收到：" + text
+
+
+
+    configuration = Configuration(
+        access_token=
+        LINE_CHANNEL_ACCESS_TOKEN
+    )
+
+
+    with ApiClient(configuration) as api_client:
+
+        api = MessagingApi(api_client)
+
+
+        api.reply_message(
+            ReplyMessageRequest(
+
+                reply_token=
+                event.reply_token,
+
+
+                messages=[
+                    TextMessage(
+                        text=reply
+                    )
+                ]
+
+            )
+        )
