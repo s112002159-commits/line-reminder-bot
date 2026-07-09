f94ea85b5bca09cf1b00dabd432cb1d45b6981b9
s112002159-commits <s112002159@g.ksu.edu.tw>
2026-07-09 10:45:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,94 @@
-import os
-from flask import Flask, request, jsonify
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
+from flask import Flask, request, abort
+
+from linebot.v3.webhook import (
+    WebhookHandler
+)
+
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
 from line_handler import handle_event
 
+from config import (
+    LINE_CHANNEL_SECRET
+)
+
+
 app = Flask(__name__)
-line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN',''))
-handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET',''))
 
-@app.route('/callback', methods=['POST'])
+
+handler = WebhookHandler(
+    LINE_CHANNEL_SECRET
+)
+
+
+@handler.add(
+    MessageEvent,
+    message=TextMessageContent
+)
+def message_event(event):
+
+    handle_event(event)
+
+
+
+@app.route("/")
+def home():
+
+    return "LINE Reminder Bot Running"
+
+
+
+@app.route("/callback", methods=["POST"])
 def callback():
-    signature=request.headers.get('X-Line-Signature','')
-    body=request.get_data(as_text=True)
+
+    signature = request.headers.get(
+        "X-Line-Signature"
+    )
+
+    body = request.get_data(
+        as_text=True
+    )
+
+
     try:
-        handler.handle(body, signature)
+
+        handler.handle(
+            body,
+            signature
+        )
+
+
     except InvalidSignatureError:
-        return 'Invalid signature',400
-    return 'OK'
 
-@app.route('/')
-def index():
-    return jsonify({'status':'LINE Bot running'})
+        abort(400)
 
-handler.add(MessageEvent=None, message=None)(handle_event)
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=int(os.getenv('PORT',5000)))
+
+    return "OK"
+
+
+
+@app.route("/send-report")
+def send_report():
+
+    from scheduler import daily_report
+
+    result = daily_report()
+
+    return result
+
+
+
+if __name__ == "__main__":
+
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
