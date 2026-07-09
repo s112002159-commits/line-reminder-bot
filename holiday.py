bc1468ee88dd2eb439d20a43d4bbd39af079eb1f
s112002159-commits <s112002159@g.ksu.edu.tw>
2026-07-09 12:14:07 +0800

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -1,33 +1,27 @@
 import holidays
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 
 
 tw_holidays = holidays.Taiwan()
 
 
-
-def is_workday(date=None):
-
-    if date is None:
-        date = datetime.now()
-
-
-    # 六日
-    if date.weekday() >= 5:
+def is_workday(target_date):
+    """
+    判斷指定日期是否工作日
+    """
+    if target_date.weekday() >= 5:
         return False
 
-
-    # 國定假日
-    if date.date() in tw_holidays:
+    if target_date in tw_holidays:
         return False
-
 
     return True
 
 
-
 def is_tomorrow_workday():
-
-    tomorrow = datetime.now() + timedelta(days=1)
+    """
+    判斷明天是否上班
+    """
+    tomorrow = date.today() + timedelta(days=1)
 
     return is_workday(tomorrow)
