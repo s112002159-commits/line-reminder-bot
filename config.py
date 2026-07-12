8106e6f3d830c6143cab338406382843951d382e
s112002159-commits <s112002159@g.ksu.edu.tw>
2026-07-12 22:56:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,64 @@
 import os
 
+# ============================
+# LINE
+# ============================
 
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv(
     "LINE_CHANNEL_ACCESS_TOKEN"
 )
 
-
 LINE_CHANNEL_SECRET = os.getenv(
     "LINE_CHANNEL_SECRET"
 )
+
+# ============================
+# 管理員
+# Render可設定多位
+#
+# ADMIN_USERS=
+# Uxxxxxxxx,Uyyyyyyyy
+# ============================
+
+ADMIN_USERS = os.getenv(
+    "ADMIN_USERS",
+    ""
+).split(",")
+
+# ============================
+# DATA FILE
+# ============================
+
+DATA_FILE = "data.json"
+
+# ============================
+# 成員名單
+# ============================
+
+DEFAULT_MEMBERS = [
+
+    "造賓",
+
+    "佳真",
+
+    "宗旂",
+
+    "培昇",
+
+    "季家",
+
+    "佳峻",
+
+    "彥呈",
+
+    "欣雯"
+
+]
+
+# ============================
+# BOT VERSION
+# ============================
+
+VERSION = "v2.0.0"
+
+BUILD = "2026.07"
