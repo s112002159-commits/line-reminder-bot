dd41101331307d842f51249bd5d0660b77071b25
s112002159-commits <s112002159@g.ksu.edu.tw>
2026-07-24 10:36:39 +0800

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -1,15 +1,23 @@
-import holidays
-import datetime
-
+# =====================
+# 台灣假日
+# =====================
 tw_holidays = holidays.Taiwan()
 
 def is_tomorrow_workday():
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 
+    tomorrow = (
+        taiwan_now().date()
+        + datetime.timedelta(days=1)
+    )
+
+    # 六日
     if tomorrow.weekday() >= 5:
+
         return False
 
+    # 國定假日
     if tomorrow in tw_holidays:
+
         return False
 
     return True
